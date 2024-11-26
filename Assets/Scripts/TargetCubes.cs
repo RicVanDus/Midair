e98d16252f321d1cacc5e5f3dd204ec004639b4b
Richard van Dusschoten <richardd@mediamonks.com>
2024-11-26 16:38:28 +0100

    Added simple environment and physics test script to cube

---
+++ Assets/Scripts/TargetCubes.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCubes : MonoBehaviour
+{
+
+    private float _timer = 0f;
+    private Rigidbody _rigidbody;
+    
+    public Vector3 forceDir;
+    public float forceStrength;
+    
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer > 3f)
+        {
+            Force(forceDir, forceStrength);
+            _timer = 0f;
+        }
+    }
+
+    void Force(Vector3 forceDir, float forceStrength)
+    {
+        Vector3 completeForce;
+
+        completeForce = forceDir * forceStrength;
+        
+        if (_rigidbody != null)
+            _rigidbody.AddForce(completeForce);
+    }
+}
