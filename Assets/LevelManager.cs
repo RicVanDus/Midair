1e7d314969e36841e41e4d6ef93b0bd3b8298fe2
Rivandu <richardvandusschoten@gmail.com>
2024-12-21 20:41:26 +0100

    Added empty levelmanager script, added a platform to the level

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public float gravityForce;
+    public float gravityForceDefault = 100f;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
