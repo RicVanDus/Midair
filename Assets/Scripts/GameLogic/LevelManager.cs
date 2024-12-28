c1e12664958e1afb69887476b697f1dfad73188a
Rivandu <richardvandusschoten@gmail.com>
2024-12-28 22:36:47 +0100

    Levelmanager now spawns enemies (cubes) on random places

--- Assets/Scripts/GameLogic/LevelManager.cs
+++ Assets/Scripts/GameLogic/LevelManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -9,14 +10,13 @@ public class LevelManager : MonoBehaviour
     public float gravityForceDefault = 100f;
 
 
-    private EnemyBase[] currentEnemies;
     public int maxEnemyCount;
+    private List<GameObject> _enemyList = new List<GameObject>();
     [SerializeField] private GameObject _targetCube;
-
     [SerializeField] private Transform _spawnBounds;
 
     //misc
-    private WaitForSeconds _waitCheckEnemyList = new WaitForSeconds(2f);
+    private WaitForSeconds _waitCheckEnemyList = new(3f);
 
     void Start()
     {
@@ -39,20 +39,35 @@ public class LevelManager : MonoBehaviour
         // randomSpawn position through levelbounds
         Vector3 randomSpawnPos = Vector3.zero;
         float levelBoundsX = _spawnBounds.localScale.x / 2;
-        float levelBoundsY = _spawnBounds.localScale.y / 2 + _spawnBounds.transform.position.y;
+        float levelBoundsYmin = (_spawnBounds.localScale.y / 2 *-1) + _spawnBounds.transform.position.y;
+        float levelBoundsYmax = (_spawnBounds.localScale.y / 2) + _spawnBounds.transform.position.y;
         float rndSpawnX = Random.Range(-(levelBoundsX), levelBoundsX);
-        float rndSpawnY = Random.Range(-(levelBoundsY), levelBoundsY);
+        float rndSpawnY = Random.Range(levelBoundsYmin, levelBoundsYmax);
         randomSpawnPos.x = rndSpawnX;
         randomSpawnPos.y = rndSpawnY;
 
-        Instantiate(_targetCube, randomSpawnPos, Quaternion.identity);
+        var newEnemy = Instantiate(_targetCube, randomSpawnPos, Quaternion.identity);
+        
+        _enemyList.Add(newEnemy);
+    }
+
+    public void ThisDestroyedEnemy(EnemyBase nme)
+    {
+        // delete from list
+        _enemyList.Remove(nme.gameObject);
     }
 
     private IEnumerator checkEnemyList()
     {
         // if enemylist.count < max enemy count then spawn one
-        SpawnEnemy();
+        do
+        {
+            if (_enemyList.Count < maxEnemyCount)
+            {
+                SpawnEnemy();                
+            }
 
-        yield return _waitCheckEnemyList;
+            yield return _waitCheckEnemyList;
+        } while (true);
     }
 }
